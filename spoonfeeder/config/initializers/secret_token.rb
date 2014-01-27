ddb2d323db517cb75e943e57e9c831bb64e28983
alcesleo <lagginglion@gmail.com>
2014-01-27 17:45:27 +0100

    Moved all files up one step

--- spoonfeeder/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Spoonfeeder::Application.config.secret_token = '46085a5b6b8dbf0e609ab3cc8d4a514df3048c51bff496ac4e6a82ef5c9127266da6016bc9656c7665906a36aac0e93119e59f601fca3a0d67ce32073ce28a52'
