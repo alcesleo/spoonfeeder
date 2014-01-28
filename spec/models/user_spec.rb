922eedd653a5a735d0f95372bffd29737ad0aae0
alcesleo <lagginglion@gmail.com>
2014-01-28 12:34:21 +0100

    Test stubs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with a username and password" do
+    user = User.new(
+      name: 'Username',
+      password: 'Password',
+      password_confirmation: 'Password')
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a username"
+  it "is invalid without a password"
+  it "is invalid if it is a duplicate"
+  it "always has at least one admin user remaining"
 end
