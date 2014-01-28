8038e35a2de6d579041d3267aa0239efdc3b9dbb
alcesleo <lagginglion@gmail.com>
2014-01-28 14:27:49 +0100

    User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
 
-  it "is valid with a username and password" do
+  it "is valid with a username and confirmed password" do
     user = User.new(
       name: 'Username',
       password: 'Password',
@@ -10,8 +10,43 @@ describe User do
     expect(user).to be_valid
   end
 
-  it "is invalid without a username"
-  it "is invalid without a password"
-  it "is invalid if it is a duplicate"
-  it "always has at least one admin user remaining"
+  it "is invalid without a username" do
+    expect(User.new(name: nil)).to have(1).errors_on(:name)
+  end
+
+  it "is invalid without a password" do
+    expect(User.new(password: nil)).to have(1).errors_on(:password)
+  end
+
+  it "is invalid without a password confirmation" do
+    expect(User.new(password_confirmation: nil)).to have(1).errors_on(:password_confirmation)
+  end
+
+  it "is invalid if it is a duplicate" do
+    user = User.new(
+      name: 'Username',
+      password: 'Password',
+      password_confirmation: 'Password')
+
+    user.save
+
+    duplicate_user = User.new(
+      name: 'Username',
+      password: 'Password',
+      password_confirmation: 'Password')
+
+    expect(duplicate_user).to be_invalid
+
+  end
+
+  it "always has at least one admin user remaining" do
+    # create user
+    user = User.create(
+      name: 'Username',
+      password: 'Password',
+      password_confirmation: 'Password')
+
+    user.delete
+    expect(User.all.count).to be 1
+  end
 end
