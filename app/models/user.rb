d205ff6cf01bac8e37d6da905d3f9f399c48fc30
alcesleo <lagginglion@gmail.com>
2014-01-28 10:55:00 +0100

    Nicer indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ActiveRecord::Base
 
   after_destroy :ensure_one_admin_remains
 
-  private
-    def ensure_one_admin_remains
-      if User.count.zero?
-        raise "Can't delete last user"
-      end 
+  private def ensure_one_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
     end
+  end
 
 end
